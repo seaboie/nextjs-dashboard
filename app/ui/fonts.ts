9b9966eaf34bd48a20d86b045dfe81c976c659cd
kritbovorn <kritbovorn@gmail.com>
2024-03-07 19:20:59 +0700

    finish: chapter3

---
+++ app/ui/fonts.ts
@@ -0,0 +1,14 @@
+import { Kanit, Lusitana } from 'next/font/google';
+
+export const kanit = Kanit({
+     subsets: ['latin'],
+     weight: ['400'] 
+    });
+
+export const lusitana = Lusitana({
+    subsets: ['latin'],
+    weight: ['400', '700'],
+});
+
+
+
